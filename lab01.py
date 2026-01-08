2f0f80bc0f7113b98cf1898a2d6f3aa2a5040fe6
Chris Everhart <chris.everhart@intele-gents.com>
2026-01-07 21:04:30 -0500

    init commit

---
+++ lab01.py
@@ -0,0 +1,9 @@
+x = 5
+y = 3
+z = x + y
+print(z) # Output: 8
+
+a = int(input("Enter number1: ")) # Input example: 10
+b = int(input("Enter number2: ")) # Input example: 2
+c = a / b
+print(c) # Output: 5.0
