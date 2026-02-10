6fadedfb14b66c27ca78a4d685d85c35847b8c73
Chris Everhart <chris.everhart@furman.edu>
2026-02-10 08:49:00 -0500

    Use text split to print the values cleanly

--- Module 6/lab06-graded.py
+++ Module 6/lab06-graded.py
@@ -1,3 +1,6 @@
+employee_data = {}
+
 with open("emp_data.txt", 'r') as file:
     for line in file:
-        print(line.strip())
+        print("Employee ID:", line.split(", ")[0])
+        print("Name:", line.split(", ")[1])
