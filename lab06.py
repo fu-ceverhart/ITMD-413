3eed525ac6f539cf07387c03769f04c32f0a8735
Chris Everhart <chris.everhart@furman.edu>
2026-02-10 08:16:28 -0500

    initialize dictionary and a correct counter

---
+++ lab06.py
@@ -0,0 +1,10 @@
+states_and_capitals = {
+    "California": "Sacramento",
+    "Texas": "Austin",
+    "Florida": "Tallahassee",
+    "New York": "Albany",
+    "Colorado": "Denver"
+}
+
+correct = 0
+
