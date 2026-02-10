4ad8d5270c962245260bdbcf149841fbb3d80f0d
Chris Everhart <chris.everhart@furman.edu>
2026-02-10 09:02:46 -0500

    Add enumeration to the for loop for record processing.

--- Module 6/lab06-graded.py
+++ Module 6/lab06-graded.py
@@ -1,6 +1,7 @@
 employee_data = {}
 
 with open("emp_data.txt", 'r') as file:
-    for line in file:
+    for i, line in enumerate(file, start=1):
+        print(i)
         print("Employee ID:", line.split(", ")[0])
         print("Name:", line.split(", ")[1])
