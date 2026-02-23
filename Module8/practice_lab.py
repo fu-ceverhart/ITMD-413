6d6dc06d44ba8b37cff6230ecd971fc129bbfb89
Chris Everhart <chris.everhart@furman.edu>
2026-02-23 08:50:57 -0500

    add the Gallons section for users to enter their information.

--- Module8/practice_lab.py
+++ Module8/practice_lab.py
@@ -5,6 +5,10 @@ class GUI:
     '''Main application window class'''
     def __init__(self):
         self.main_window = tk.Tk()
+        self.gallons_label = tk.Label(self.main_window, text="Gallons:")
+        self.gallons_entry = tk.Entry(self.main_window)
+        self.gallons_label.pack()
+        self.gallons_entry.pack()
         self.main_window.mainloop()
 
 app = GUI()
