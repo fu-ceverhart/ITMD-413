3f8c1ce43035fdbf85e0898bb50687bcfb5053f5
Chris Everhart <chris.everhart@furman.edu>
2026-02-23 08:54:38 -0500

    Add the option for the user to enter Miles

--- Module8/practice_lab.py
+++ Module8/practice_lab.py
@@ -5,11 +5,16 @@ class GUI:
     '''Main application window class'''
     def __init__(self):
         self.main_window = tk.Tk()
-        self.main_window.geometry("640x480") #Looked this up on geeks for geeks. I didn't like now small the default window was.
+        #Looked this up on geeks for geeks. I didn't like now small the default window was.
+        self.main_window.geometry("640x480") 
         self.gallons_label = tk.Label(self.main_window, text="Gallons:")
         self.gallons_entry = tk.Entry(self.main_window)
         self.gallons_label.pack()
         self.gallons_entry.pack()
+        self.miles_label = tk.Label(self.main_window, text="Miles:")
+        self.miles_entry = tk.Entry(self.main_window)
+        self.miles_label.pack()
+        self.miles_entry.pack()
         self.main_window.mainloop()
 
 app = GUI()
